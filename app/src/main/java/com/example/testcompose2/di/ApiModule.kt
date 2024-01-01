4e7686ded00e3a001116c3204c1c7b2bf63f6d92
jeeminimini <jiminn821@naver.com>
2024-01-02 01:40:17 +0900

    build : di 관리 파일 생성

---
+++ app/src/main/java/com/example/testcompose2/di/ApiModule.kt
@@ -0,0 +1,10 @@
+package com.example.testcompose2.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApiModule {
+}
