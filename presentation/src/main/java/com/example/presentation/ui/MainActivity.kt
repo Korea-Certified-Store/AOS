53110221a5457b14621d40ad4cbbfbd7343137d6
jeeminimini <jiminn821@naver.com>
2024-01-07 00:08:00 +0900

    feat : store summary bottom sheet layout 구현

--- presentation/src/main/java/com/example/presentation/ui/MainActivity.kt
+++ presentation/src/main/java/com/example/presentation/ui/MainActivity.kt
@@ -3,13 +3,42 @@ package com.example.presentation.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.BottomSheetScaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import com.example.presentation.ui.theme.Android_KCSTheme
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.presentation.R
+import com.example.presentation.ui.theme.LightBlue
+import com.example.presentation.ui.theme.LightGray
+import com.example.presentation.ui.theme.MediumBlue
+import com.example.presentation.ui.theme.MediumGray
+import com.example.presentation.ui.theme.Red
 import com.naver.maps.map.compose.ExperimentalNaverMapApi
 import com.naver.maps.map.compose.NaverMap
 
@@ -18,29 +47,102 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Android_KCSTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MainScreen()
-                }
-            }
+            MainScreen()
+            StoreSummaryBottomSheet()
         }
     }
 }
 
-@ExperimentalNaverMapApi
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun MainScreen(modifier: Modifier = Modifier) {
-    Box(
-        modifier = modifier
+@Preview
+fun StoreSummaryBottomSheet() {
+    BottomSheetScaffold(
+        sheetContent = {
+            Column {
+                StoreSummaryInfo()
+            }
+        },
+        sheetPeekHeight = 200.dp,
+        sheetContainerColor = Color.White,
+        sheetShape = RoundedCornerShape(topStart = 15.dp, topEnd = 15.dp),
+        sheetShadowElevation = 5.dp
     ) {
-        NaverMap (
-            modifier = Modifier.fillMaxSize()
-        ) {
 
+    }
+}
+
+
+@Preview
+@Composable
+fun StoreSummaryInfo() {
+    Row(modifier = Modifier
+        .padding(horizontal = 16.dp)
+        .fillMaxWidth(1f),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Column(
+            horizontalAlignment = Alignment.Start
+        ){
+            Text(text = "미진일식", color = MediumBlue, fontSize = 18.sp, fontWeight = FontWeight.ExtraBold)
+            Spacer(modifier = Modifier.padding(bottom = 2.dp))
+            Text(text = "일식", color = MediumGray, fontSize = 10.sp, fontWeight = FontWeight.Normal)
+            Spacer(modifier = Modifier.padding(bottom = 6.dp))
+            Text(text = "영업 중", color = Red, fontSize = 11.sp, fontWeight = FontWeight.Normal)
+            Spacer(modifier = Modifier.height(17.dp))
+            StoreCallButton()
+            Spacer(modifier = Modifier.height(16.dp))
+        }
+        Column{
+            StoreImage()
         }
     }
+}
+
+@Preview
+@Composable
+fun StoreCallButton() {
+    Button(
+        onClick = {},
+        modifier = Modifier
+            .defaultMinSize(minWidth = 1.dp, minHeight = 1.dp),
+        contentPadding = PaddingValues(horizontal = 25.dp, vertical = 10.dp),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = LightGray
+        )
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.call),
+            tint = LightBlue,
+            contentDescription = "Call",
+            modifier = Modifier.size(21.dp)
+        )
+    }
+
+}
+
+@Preview
+@Composable
+fun StoreImage() {
+    Card(
+        modifier = Modifier.size(116.dp),
+        shape = RoundedCornerShape(6.dp)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.store_example),
+            contentDescription = "Store Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize(1f)
+        )
+    }
+}
+
+
+@ExperimentalNaverMapApi
+@Composable
+fun MainScreen() {
+    NaverMap (
+        modifier = Modifier.fillMaxSize()
+    )
 }
 
