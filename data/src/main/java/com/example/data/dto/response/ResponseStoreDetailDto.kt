bdc214329688f7e697bfcee5d2afd94298af854b
somin <salth0601@gmail.com>
2024-01-17 08:18:41 +0900

    chore : 불필요한 @SerialName 어노테이션 삭제

--- data/src/main/java/com/example/data/dto/response/ResponseStoreDetailDto.kt
+++ data/src/main/java/com/example/data/dto/response/ResponseStoreDetailDto.kt
@@ -4,80 +4,55 @@ import com.example.domain.model.Coordinate
 import com.example.domain.model.OpeningHours
 import com.example.domain.model.StoreDetail
 import com.example.domain.model.TimeInfo
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class ResponseStoreDetailDto(
-    @SerialName("id")
     val id: Long,
-    @SerialName("displayName")
     val displayName: String,
-    @SerialName("primaryTypeDisplayName")
     val primaryTypeDisplayName: String?,
-    @SerialName("formattedAddress")
     val formattedAddress: String,
-    @SerialName("phoneNumber")
     val phoneNumber: String?,
-    @SerialName("location")
     val location: CoordinateData,
-    @SerialName("regularOpeningHours")
     val regularOpeningHours: List<OpeningHoursData>,
-    @SerialName("localPhotos")
     val localPhotos: List<String?>,
-    @SerialName("certificationName")
     val certificationName: List<String>
 ) {
     @Serializable
     data class CoordinateData(
-        @SerialName("longitude")
         val longitude: Double,
-        @SerialName("latitude")
         val latitude: Double,
     ) {
-        fun toCoordinate() = run {
-            Coordinate(longitude, latitude)
-        }
+        fun toDomainModel(): Coordinate = Coordinate(longitude = longitude, latitude = latitude)
     }
 
     @Serializable
     data class OpeningHoursData(
-        @SerialName("open")
         val open: TimeInfoData,
-        @SerialName("close")
         val close: TimeInfoData,
     ) {
-        fun toOpeningHours() = run {
-            OpeningHours(open.toTimeInfo(), close.toTimeInfo())
-        }
-
-        @Serializable
-        data class TimeInfoData(
-            @SerialName("day")
-            val day: String,
-            @SerialName("hour")
-            val hour: Int,
-            @SerialName("minute")
-            val minute: Int,
-        ) {
-
-            fun toTimeInfo() = run {
-                TimeInfo(day, hour, minute)
-            }
-        }
+        fun toDomainModel(): OpeningHours =
+            OpeningHours(open.toDomainTimeInfoModel(), close.toDomainTimeInfoModel())
     }
 
-    fun toStoreDetail() = run {
-        StoreDetail(
-            id,
-            displayName,
-            primaryTypeDisplayName,
-            formattedAddress,
-            phoneNumber,
-            location.toCoordinate(),
-            regularOpeningHours.map { it.toOpeningHours() },
-            localPhotos,
-            certificationName
-        )
+    @Serializable
+    data class TimeInfoData(
+        val day: String,
+        val hour: Int,
+        val minute: Int,
+    ) {
+        fun toDomainTimeInfoModel(): TimeInfo = TimeInfo(day = day, hour = hour, minute = minute)
     }
+
+    fun toStoreDetail(): StoreDetail = StoreDetail(
+        id = id,
+        displayName = displayName,
+        primaryTypeDisplayName = primaryTypeDisplayName,
+        formattedAddress = formattedAddress,
+        phoneNumber = phoneNumber,
+        location = location.toDomainModel(),
+        regularOpeningHours = regularOpeningHours.map { openingHoursData -> openingHoursData.toDomainModel() },
+        localPhotos = localPhotos,
+        certificationName = certificationName
+    )
 }
