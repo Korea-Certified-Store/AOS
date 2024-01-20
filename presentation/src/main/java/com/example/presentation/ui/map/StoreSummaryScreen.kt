0ef46bcc4f1685d64b4a71e22f1cec07d058dadf
jeeminimini <jiminn821@naver.com>
2024-01-20 17:54:24 +0900

    refactor : bottomSheet Constraint Layout으로 마이그레이션

--- presentation/src/main/java/com/example/presentation/ui/map/StoreSummaryScreen.kt
+++ presentation/src/main/java/com/example/presentation/ui/map/StoreSummaryScreen.kt
@@ -3,7 +3,7 @@ package com.example.presentation.ui.map
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.BottomSheetScaffold
@@ -28,19 +29,29 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
 import com.example.presentation.R
+import com.example.presentation.model.Coordinate
 import com.example.presentation.model.Day
 import com.example.presentation.model.OpeningHours
 import com.example.presentation.model.OperatingTime
@@ -57,6 +68,9 @@ import com.example.presentation.ui.theme.MediumGray
 import com.example.presentation.ui.theme.Pink
 import com.example.presentation.ui.theme.Red
 import com.example.presentation.ui.theme.White
+import com.example.presentation.util.MainConstants.BOTTOM_SHEET_HEIGHT_OFF
+import com.example.presentation.util.MainConstants.BOTTOM_SHEET_STORE_IMG_SIZE
+import com.example.presentation.util.MainConstants.DEFAULT_MARIN
 import java.util.Calendar
 import java.util.Date
 import kotlin.math.abs
@@ -91,7 +105,6 @@ fun StoreSummaryBottomSheet(
         },
         sheetSwipeEnabled = false
     ) {
-
     }
 }
 
@@ -100,40 +113,76 @@ fun StoreSummaryInfo(
     storeInfo: StoreDetail,
     onCallDialogChanged: (Boolean) -> Unit
 ) {
-    Row(
-        modifier = Modifier
-            .padding(horizontal = 16.dp)
-            .fillMaxWidth(1f),
-        horizontalArrangement = Arrangement.SpaceBetween
-    ) {
-        Column(
-            horizontalAlignment = Alignment.Start
+    BoxWithConstraints {
+        ConstraintLayout(
+            modifier = Modifier
+                .padding(horizontal = DEFAULT_MARIN.dp, vertical = 13.dp)
+                .fillMaxWidth(1f)
+                .wrapContentHeight(),
+            constraintSet = bottomSheetConstraints()
         ) {
-            Spacer(modifier = Modifier.height(12.dp))
-            StoreTitle(storeInfo.displayName, storeInfo.primaryTypeDisplayName ?: "상점")
-            Spacer(modifier = Modifier.height(8.dp))
-            Chips(storeInfo.certificationName)
-            Spacer(modifier = Modifier.height(8.dp))
-            StoreOpeningTime(storeInfo.regularOpeningHours)
-            Spacer(modifier = Modifier.height(11.dp))
-            StoreCallButton(onCallDialogChanged)
-            Spacer(modifier = Modifier.height(12.dp))
+            StoreTitle(
+                storeInfo.displayName,
+                storeInfo.primaryTypeDisplayName ?: "상점",
+                "storeTitle",
+                maxWidth
+            )
+            Chips(storeInfo.certificationName, "chips")
+            StoreOpeningTime(storeInfo.regularOpeningHours, "storeOpeningTime")
+            StoreCallButton(onCallDialogChanged, "storeCallButton")
+            StoreImage("storeImage")
         }
-        Column {
-            Spacer(modifier = Modifier.height(12.dp))
-            StoreImage()
+    }
+}
+
+fun bottomSheetConstraints(): ConstraintSet {
+    return ConstraintSet {
+        val storeTitle = createRefFor("storeTitle")
+        val chips = createRefFor("chips")
+        val storeOpeningTime = createRefFor("storeOpeningTime")
+        val storeCallButton = createRefFor("storeCallButton")
+        val storeImage = createRefFor("storeImage")
+
+        constrain(storeTitle) {
+            top.linkTo(parent.top)
+            linkTo(start = parent.start, end = storeImage.start, endMargin = 100.dp, bias = 0F)
+        }
+        constrain(chips) {
+            start.linkTo(parent.start)
+            top.linkTo(storeTitle.bottom, 4.dp)
+        }
+        constrain(storeOpeningTime) {
+            start.linkTo(parent.start)
+            top.linkTo(chips.bottom, 3.dp)
+        }
+        constrain(storeCallButton) {
+            start.linkTo(parent.start)
+            top.linkTo(storeOpeningTime.bottom, 5.dp)
+            bottom.linkTo(parent.bottom)
+        }
+        constrain(storeImage) {
+            end.linkTo(parent.end)
+            top.linkTo(parent.top)
         }
     }
 }
 
 @Composable
-fun StoreTitle(storeName: String, storeType: String) {
-    Row(verticalAlignment = Alignment.CenterVertically) {
+fun StoreTitle(storeName: String, storeType: String, id: String, maxWidth: Dp) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .layoutId(id)
+            .width(maxWidth - BOTTOM_SHEET_STORE_IMG_SIZE.dp - (DEFAULT_MARIN * 2).dp)
+    ) {
         Text(
             text = storeName,
             color = MediumBlue,
-            fontSize = 22.sp,
-            fontWeight = FontWeight.Bold
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis,
+            modifier = Modifier.layoutId(id),
         )
         Spacer(modifier = Modifier.width(8.dp))
         Text(
@@ -146,7 +195,7 @@ fun StoreTitle(storeName: String, storeType: String) {
 }
 
 @Composable
-private fun Chip(
+fun Chip(
     storeType: StoreType
 ) {
     Surface(
@@ -169,21 +218,26 @@ private fun Chip(
 
 @Composable
 fun Chips(
-    elements: List<StoreType>
+    elements: List<StoreType>,
+    id: String
 ) {
-    LazyRow(modifier = Modifier) {
+    LazyRow(modifier = Modifier.layoutId(id)) {
         items(elements.size) { idx ->
             Chip(storeType = elements[idx])
             Spacer(modifier = Modifier.padding(4.dp))
         }
     }
+
 }
 
 @Composable
-fun StoreOpeningTime(regularOpeningHours: List<OpeningHours>) {
+fun StoreOpeningTime(
+    regularOpeningHours: List<OpeningHours>,
+    id: String
+) {
     val operatingTime = getOperatingType(regularOpeningHours)
 
-    Row {
+    Row(modifier = Modifier.layoutId(id)) {
         Text(
             text = stringResource(operatingTime.operatingType.description),
             Modifier.alignByBaseline(),
@@ -279,7 +333,10 @@ fun calculateOperating(
             "${operatingTimes.last().close.hour}:${getOperatingMinute(operatingTimes.last().close.minute)}에 영업 종료"
         )
     } else if (nowTime.hour * 60 + nowTime.minute < operatingTimes.last().open.hour * 60 + operatingTimes.last().open.minute) {
-        return OperatingTime(OperatingType.CLOSED, "${operatingTimes.last().open.hour}:${getOperatingMinute(operatingTimes.last().open.minute)}에 영업 시작")
+        return OperatingTime(
+            OperatingType.CLOSED,
+            "${operatingTimes.last().open.hour}:${getOperatingMinute(operatingTimes.last().open.minute)}에 영업 시작"
+        )
     }
 
     return OperatingTime(OperatingType.CLOSED, getNextDayOpeningHour(nextDay))
@@ -289,13 +346,18 @@ fun getCloseHour(hour: Int) = if (hour == 0) 24 else hour
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun StoreCallButton(onCallDialogChanged: (Boolean) -> Unit) {
+fun StoreCallButton(
+    onCallDialogChanged: (Boolean) -> Unit,
+    id: String
+) {
     CompositionLocalProvider(LocalMinimumInteractiveComponentEnforcement provides false) {
         Button(
             onClick = {
                 onCallDialogChanged(true)
             },
-            modifier = Modifier.defaultMinSize(minWidth = 1.dp, minHeight = 1.dp),
+            modifier = Modifier
+                .defaultMinSize(minWidth = 1.dp, minHeight = 1.dp)
+                .layoutId(id),
             contentPadding = PaddingValues(horizontal = 20.dp, vertical = 1.dp),
             colors = ButtonDefaults.buttonColors(containerColor = White),
             shape = RoundedCornerShape(3.dp),
@@ -311,11 +373,12 @@ fun StoreCallButton(onCallDialogChanged: (Boolean) -> Unit) {
     }
 }
 
-@Preview
 @Composable
-fun StoreImage() {
+fun StoreImage(id: String) {
     Card(
-        modifier = Modifier.size(116.dp),
+        modifier = Modifier
+            .size(BOTTOM_SHEET_STORE_IMG_SIZE.dp)
+            .layoutId(id),
         shape = RoundedCornerShape(6.dp),
         border = BorderStroke(0.3.dp, LightGray)
     ) {
--- presentation/src/main/java/com/example/presentation/util/MainConstants.kt
+++ presentation/src/main/java/com/example/presentation/util/MainConstants.kt
@@ -6,4 +6,6 @@ object MainConstants {
     const val SEARCH_ON_CURRENT_MAP_BUTTON_DEFAULT_PADDING = 16
     const val LONG_LIMIT = 0.0375
     const val LAT_LIMIT = 0.04
+    const val BOTTOM_SHEET_STORE_IMG_SIZE = 122
+    const val DEFAULT_MARIN = 16
 }
