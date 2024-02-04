184c19732c64bbf0a4e6976c8255560cbcb8991a
jeeminimini <jiminn821@naver.com>
2024-02-04 15:03:31 +0900

    feat : StoreListDivider 구현

--- presentation/src/main/java/com/example/presentation/ui/map/list/StoreListComponent.kt
+++ presentation/src/main/java/com/example/presentation/ui/map/list/StoreListComponent.kt
@@ -4,6 +4,9 @@ import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -18,28 +21,66 @@ import com.example.presentation.ui.component.StoreImageCard
 import com.example.presentation.ui.component.StoreTitleText
 import com.example.presentation.ui.component.StorePrimaryTypeText
 import com.example.presentation.ui.component.StoreTypeChips
+import com.example.presentation.ui.theme.SemiLightGray
 import com.example.presentation.util.MainConstants.BOTTOM_SHEET_STORE_LIST_IMG_SIZE
 import com.example.presentation.util.MainConstants.DEFAULT_MARGIN
 
 @Preview
 @Composable
 fun test() {
-    StoreListItem(
-        storeInfo = StoreDetail(
-            id = 0,
-            displayName = "성산일출봉 청운식당Cheongwon",
-            primaryTypeDisplayName = "일본 음식점",
-            formattedAddress = "주소",
-            phoneNumber = "전화",
-            location = Coordinate(latitude = 0.0, longitude = 0.0),
-            operatingType = "타입",
-            timeDescription = "타임",
-            localPhotos = listOf(),
-            certificationName = listOf(StoreType.GREAT, StoreType.KIND, StoreType.SAFE),
-            operationTimeOfWeek = mapOf()
-        )
-    )
+    LazyColumn {
+        itemsIndexed(
+            listOf(
+                StoreDetail(
+                    id = 0,
+                    displayName = "성산일출봉 청운식당Cheongwon",
+                    primaryTypeDisplayName = "일본 음식점",
+                    formattedAddress = "주소",
+                    phoneNumber = "전화",
+                    location = Coordinate(latitude = 0.0, longitude = 0.0),
+                    operatingType = "타입",
+                    timeDescription = "타임",
+                    localPhotos = listOf(),
+                    certificationName = listOf(StoreType.GREAT, StoreType.KIND, StoreType.SAFE),
+                    operationTimeOfWeek = mapOf()
+
+                ),
+                StoreDetail(
+                    id = 0,
+                    displayName = "성산일출봉 청운식당Cheongwon",
+                    primaryTypeDisplayName = "일본 음식점",
+                    formattedAddress = "주소",
+                    phoneNumber = "전화",
+                    location = Coordinate(latitude = 0.0, longitude = 0.0),
+                    operatingType = "타입",
+                    timeDescription = "타임",
+                    localPhotos = listOf(),
+                    certificationName = listOf(StoreType.GREAT, StoreType.KIND, StoreType.SAFE),
+                    operationTimeOfWeek = mapOf()
+
+                ),
+                StoreDetail(
+                    id = 0,
+                    displayName = "성산일출봉 청운식당Cheongwon",
+                    primaryTypeDisplayName = "일본 음식점",
+                    formattedAddress = "주소",
+                    phoneNumber = "전화",
+                    location = Coordinate(latitude = 0.0, longitude = 0.0),
+                    operatingType = "타입",
+                    timeDescription = "타임",
+                    localPhotos = listOf(),
+                    certificationName = listOf(StoreType.GREAT, StoreType.KIND, StoreType.SAFE),
+                    operationTimeOfWeek = mapOf()
+                )
+
+            )
+        ) { idx, item ->
+            StoreListItem(storeInfo = item)
+            StoreListDivider()
+        }
+    }
 }
+
 
 @Composable
 fun StoreListItem(
@@ -56,7 +97,7 @@ fun StoreListItem(
             StoreTitleText(storeInfo.displayName, 18, "storeTitle")
             StorePrimaryTypeText(storeInfo.primaryTypeDisplayName ?: "상점", 9, "storePrimaryType")
             StoreTypeChips(storeInfo.certificationName, "chips")
-            StoreImageCard(storeInfo.localPhotos, BOTTOM_SHEET_STORE_LIST_IMG_SIZE, "storeImage", )
+            StoreImageCard(storeInfo.localPhotos, BOTTOM_SHEET_STORE_LIST_IMG_SIZE, "storeImage")
         }
     }
 }
@@ -88,4 +129,14 @@ fun setBottomSheetConstraints(): ConstraintSet {
             linkTo(top = parent.top, bottom = parent.bottom, bias = 0F)
         }
     }
+}
+
+@Composable
+fun StoreListDivider() {
+    Divider(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = DEFAULT_MARGIN.dp),
+        thickness = 0.3.dp, color = SemiLightGray
+    )
 }
