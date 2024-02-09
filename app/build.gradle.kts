2e5cd1f9710cc09b834059d45c1b4a8eab3464c8
jeeminimini <jiminn821@naver.com>
2024-02-10 02:28:58 +0900

    feat : versionName 3.1로 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 3
-        versionName = "3.0"
+        versionName = "3.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
