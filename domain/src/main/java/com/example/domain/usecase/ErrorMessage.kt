03241a39009c293537b5395bfc5f0bf798e24ae2
jeeminimini <jiminn821@naver.com>
2024-02-10 20:02:26 +0900

    feat : 검색 결과 가게 비어있을 때 ErrorMessage 수정

--- domain/src/main/java/com/example/domain/usecase/ErrorMessage.kt
+++ domain/src/main/java/com/example/domain/usecase/ErrorMessage.kt
@@ -1,7 +1,7 @@
 package com.example.domain.usecase
 
 object ErrorMessage {
-    const val ERROR_MESSAGE_STORE_IS_EMPTY = "이 지역에는 가게가 존재하지 않습니다."
+    const val ERROR_MESSAGE_STORE_IS_EMPTY = "검색 결과가 존재하지 않습니다."
     const val ERROR_MESSAGE_CHECK_INTERNET = "인터넷 연결을 확인해주세요."
     const val ERROR_MESSAGE_SERVER_IS_NOT_WORKING = "서버와의 통신이 원활하지 않습니다."
     const val ERROR_MESSAGE_UNKNOWN_ERROR = "알 수 없는 에러가 발생했습니다."
