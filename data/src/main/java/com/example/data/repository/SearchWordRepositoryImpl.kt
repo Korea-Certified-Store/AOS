49b59d251cda08be8d688f6f88582aa8118834ad
jeeminimini <jiminn821@naver.com>
2024-02-12 18:32:13 +0900

    feat : 검색어 Repository 구현

---
+++ data/src/main/java/com/example/data/repository/SearchWordRepositoryImpl.kt
@@ -0,0 +1,18 @@
+package com.example.data.repository
+
+import com.example.data.source.SearchWordDataSource
+import com.example.domain.model.search.SearchWord
+import com.example.domain.repository.SearchWordRepository
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class SearchWordRepositoryImpl @Inject constructor(private val dataSource: SearchWordDataSource) :
+    SearchWordRepository {
+    override fun getRecentSearchWords(): Flow<List<SearchWord>> {
+        return dataSource.getRecentSearchWords()
+    }
+
+    override suspend fun insertSearchWord(searchWord: SearchWord) {
+        dataSource.insertSearchWord(searchWord)
+    }
+}
